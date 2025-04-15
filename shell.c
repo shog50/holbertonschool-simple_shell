64427e60e490e28ab2192460f7008fa824bd2153
rwwwn <ralbaraiki@gmail.com>
2025-04-15 16:11:52 +0000

    Fix Task 2: Valgrind-safe, Betty-style, no-arg shell

--- shell.c
+++ shell.c
@@ -1,58 +1,52 @@
 #include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
 /**
- * run_shell - executes the shell loop
+ * run_shell - runs a simple shell loop
  */
 void run_shell(void)
 {
 char *line = NULL;
 size_t len = 0;
 ssize_t nread;
-pid_t pid;
+pid_t child_pid;
+char *argv[2];
 
 while (1)
 {
-if (isatty(STDIN_FILENO))
 write(STDOUT_FILENO, "$ ", 2);
-
 nread = getline(&line, &len, stdin);
 if (nread == -1)
 {
-free(line);
-if (isatty(STDIN_FILENO))
 write(STDOUT_FILENO, "\n", 1);
 break;
 }
-
 if (line[nread - 1] == '\n')
 line[nread - 1] = '\0';
-
+if (line[0] == '\0')
+continue;
 if (strcmp(line, "exit") == 0)
-{
-free(line);
-exit(0);
-}
+break;
 
-pid = fork();
-if (pid == -1)
+child_pid = fork();
+if (child_pid == -1)
 {
 perror("fork");
 continue;
 }
-else if (pid == 0)
+if (child_pid == 0)
 {
-char *args[2];
-args[0] = line;
-args[1] = NULL;
-if (execve(args[0], args, environ) == -1)
+argv[0] = line;
+argv[1] = NULL;
+if (execve(argv[0], argv, environ) == -1)
 {
-fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-exit(127);
+perror(argv[0]);
+exit(EXIT_FAILURE);
 }
 }
 else
