1f7adc35c91b41a2884f007da7712b078eedd3f0
rwwwn <ralbaraiki@gmail.com>
2025-04-15 16:28:21 +0000

    Task 2 Final Fix: working shell, no crash, correct output

--- shell.c
+++ shell.c
@@ -1,14 +1,10 @@
 #include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/wait.h>
 
-/**
- * run_shell - runs a simple shell loop
- */
 void run_shell(void)
 {
 char *line = NULL;
@@ -19,19 +15,25 @@ char *argv[2];
 
 while (1)
 {
+if (isatty(STDIN_FILENO))
 write(STDOUT_FILENO, "$ ", 2);
+
 nread = getline(&line, &len, stdin);
 if (nread == -1)
 {
+if (isatty(STDIN_FILENO))
 write(STDOUT_FILENO, "\n", 1);
 break;
 }
+
 if (line[nread - 1] == '\n')
 line[nread - 1] = '\0';
-if (line[0] == '\0')
-continue;
+
 if (strcmp(line, "exit") == 0)
 break;
+
+if (line[0] == '\0')
+continue;
 
 child_pid = fork();
 if (child_pid == -1)
@@ -39,6 +41,7 @@ if (child_pid == -1)
 perror("fork");
 continue;
 }
+
 if (child_pid == 0)
 {
 argv[0] = line;
@@ -52,5 +55,6 @@ exit(EXIT_FAILURE);
 else
 wait(NULL);
 }
+
 free(line);
 }
