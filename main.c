5e4e72c0c69fe0be40be6636ddb93aee51d84d2d
shog50 <10709@holbertonstudents.com>
2025-04-15 11:47:53 +0000

    Betty would be proud

--- main.c
+++ main.c
@@ -1,69 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-
-#define MAX_INPUT_SIZE 1024
 
 /**
-* main - Simple Shell implementation
-* @argc: Number of arguments
-* @argv: Array of argument strings
-*
-* Return: Always 0 (Success)
+* main - Entry point
+* Description: Prints a welcome message
+* Return: Always 0
 */
-int main(int argc, char *argv[])
+int main(void)
 {
-char *input = NULL;
-size_t len = 0;
-ssize_t nread;
-char *token;
-char *delim = " \t\r\n\a";
-pid_t pid;
-int status;
-
-(void)argc;
-(void)argv;
-
-
-while (1)
-{
-
-printf("$ ");
-nread = getline(&input, &len, stdin);
-if (nread == -1)
-{
-if (feof(stdin))
-break;
-perror("getline");
-exit(EXIT_FAILURE);
-}
-
-
-token = strtok(input, delim);
-if (token == NULL)
-continue;
-pid = fork();
-if (pid == 0)
-{
-if (execve(token, &token, NULL) == -1)
-{
-perror("Error executing command");
-exit(EXIT_FAILURE);
-}
-}
-else if (pid < 0)
-{
-perror("Error creating child process");
-exit(EXIT_FAILURE);
-}
-else 
-{
-waitpid(pid, &status, 0);
-}
-}
-
-free(input);
+printf("Hello from main.c!\n");
 return (0);
 }
+
