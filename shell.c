e743fd4a2cce9ef9e056d69a87f663a83996a729
rwwwn <ralbaraiki@gmail.com>
2025-04-16 15:09:33 +0000

    Task 3: Add support for commands with arguments

--- shell.c
+++ shell.c
@@ -1,18 +1,19 @@
-#include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include "shell.h"
 
 void run_shell(void)
 {
 char *line = NULL;
 size_t len = 0;
 ssize_t nread;
-char *command;
 pid_t child_pid;
-char *argv[2];
+char *argv[MAX_ARGS];
+char *token;
+int i;
 
 while (1)
 {
@@ -22,6 +23,7 @@ write(STDOUT_FILENO, "$ ", 2);
 nread = getline(&line, &len, stdin);
 if (nread == -1)
 {
+free(line);
 if (isatty(STDIN_FILENO))
 write(STDOUT_FILENO, "\n", 1);
 break;
@@ -30,11 +32,23 @@ break;
 if (line[nread - 1] == '\n')
 line[nread - 1] = '\0';
 
-command = strtok(line, " ");
-while (command)
+if (strcmp(line, "exit") == 0)
 {
-if (strcmp(command, "exit") == 0)
+free(line);
 break;
+}
+
+if (line[0] == '\0')
+continue;
+
+i = 0;
+token = strtok(line, " ");
+while (token != NULL && i < MAX_ARGS - 1)
+{
+argv[i++] = token;
+token = strtok(NULL, " ");
+}
+argv[i] = NULL;
 
 child_pid = fork();
 if (child_pid == -1)
@@ -42,11 +56,8 @@ if (child_pid == -1)
 perror("fork");
 continue;
 }
-
 if (child_pid == 0)
 {
-argv[0] = command;
-argv[1] = NULL;
 if (execve(argv[0], argv, environ) == -1)
 {
 perror(argv[0]);
@@ -54,12 +65,8 @@ exit(EXIT_FAILURE);
 }
 }
 else
+{
 wait(NULL);
-
-command = strtok(NULL, " ");
 }
 }
-
-free(line);
 }
-
--- shell.h
+++ shell.h
@@ -1,6 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
-
+#define MAX_ARGS 64
 void run_shell(void);
 
 extern char **environ;
