704f27724ef476373bd80682b50731cb1ad9fe36
shog50 <10709@holbertonstudents.com>
2025-04-17 18:37:05 +0000

    2. Simple shell 0.1

--- shell.c
+++ shell.c
@@ -12,7 +12,6 @@ extern char **environ;
 
 void execute_command(char *command);
 int is_interactive(void);
-void process_multiple_commands(char *command);
 
 int main(void)
 {
@@ -36,31 +35,25 @@ exit(EXIT_SUCCESS);
 
 command[n_read - 1] = '\0';
 
-process_multiple_commands(command);
+execute_command(command);
 }
 
 free(command);
 return (0);
 }
 
-void process_multiple_commands(char *commands)
-{
-char *command = strtok(commands, " ");
-while (command != NULL)
-{
-execute_command(command);
-command = strtok(NULL, " ");
-}
-}
-
 void execute_command(char *command)
 {
 pid_t pid;
 int status;
-char *argv[2];
+char *argv[3];
 
-argv[0] = command;
-argv[1] = NULL;
+char *exec = strtok(command, " ");
+char *arg = strtok(NULL, " ");
+
+argv[0] = exec;
+argv[1] = arg;
+argv[2] = NULL;
 
 pid = fork();
 if (pid == -1)
@@ -68,7 +61,7 @@ return;
 
 if (pid == 0)
 {
-if (execve(command, argv, environ) == -1)
+if (execve(argv[0], argv, environ) == -1)
 {
 perror("./hsh");
 exit(EXIT_FAILURE);
