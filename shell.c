ef04baea3c69da5d6b7d08d0622167672836ebe1
shog50 <10709@holbertonstudents.com>
2025-04-17 14:05:48 +0000

    2. Simple shell 0.1

--- shell.c
+++ shell.c
@@ -1,123 +1,60 @@
-#include "shell.h"
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+extern char **environ;
+
 void run_shell(void)
 {
-char *line = NULL, *token = NULL, *path = NULL, *dir = NULL;
-char *argv[64], full_path[1024];
+char *line = NULL;
 size_t len = 0;
 ssize_t nread;
 pid_t child_pid;
-int i, found, status, last_status = 0;
+int status;
 
 while (1)
 {
-if (isatty(STDIN_FILENO)) {
-write(STDOUT_FILENO, "$ ", 2);
-}
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-nread = _getline(&line, &len);
+nread = getline(&line, &len, stdin);
+
 if (nread == -1)
 {
 if (isatty(STDIN_FILENO))
 write(STDOUT_FILENO, "\n", 1);
 free(line);
-exit(last_status);
+exit(0);
 }
 
 if (line[nread - 1] == '\n')
 line[nread - 1] = '\0';
 
-if (strlen(line) == 0 || strspn(line, " \t\n") == strlen(line))
+if (strlen(line) == 0)
 continue;
-
-if (strcmp(line, "exit") == 0)
-{
-free(line);
-exit(last_status);
-}
-
-if (strcmp(line, "env") == 0)
-{
-for (i = 0; environ[i]; i++)
-{
-write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-write(STDOUT_FILENO, "\n", 1);
-}
-continue;
-}
-
-i = 0;
-token = strtok(line, " ");
-while (token && i < 63)
-{
-argv[i++] = token;
-token = strtok(NULL, " ");
-}
-argv[i] = NULL;
-
-if (argv[0] == NULL)
-continue;
-
-if (strchr(argv[0], '/') == NULL)
-{
-path = getenv("PATH");
-if (!path)
-continue;
-path = strdup(path);
-if (!path)
-continue;
-dir = strtok(path, ":");
-found = 0;
-while (dir)
-{
-snprintf(full_path, sizeof(full_path), "%s/%s", dir, argv[0]);
-if (access(full_path, X_OK) == 0)
-{
-argv[0] = full_path;
-found = 1;
-break;
-}
-dir = strtok(NULL, ":");
-}
-free(path);
-if (!found)
-{
-perror(argv[0]);
-last_status = 127;
-continue;
-}
-}
-else if (access(argv[0], X_OK) != 0)
-{
-perror(argv[0]);
-last_status = 127;
-continue;
-}
 
 child_pid = fork();
+
 if (child_pid == -1)
 {
 perror("fork");
 continue;
 }
+
 if (child_pid == 0)
 {
-if (execve(argv[0], argv, environ) == -1)
+if (execve(line, &line, environ) == -1)
 {
-perror(argv[0]);
-exit(2);
+perror(line);
+exit(1);
 }
 }
 else
 {
 wait(&status);
-last_status = WIFEXITED(status) ? WEXITSTATUS(status) : 2;
 }
 }
 }
