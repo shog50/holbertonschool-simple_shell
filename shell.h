3d3087667267b14a2728e1821c04ab65e787bbb0
rwwwn <ralbaraiki@gmail.com>
2025-04-24 20:39:13 +0000

    complete working solution for shell tasks 0.1 - 0.6

--- shell.h
+++ shell.h
@@ -1,23 +1,19 @@
 #ifndef SHELL_H
 #define SHELL_H
-#define MAX_ARGS 64
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <stddef.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-void run_shell(void);
-ssize_t _getline(char **lineptr, size_t *n);
-extern char **environ;
-/* shell.h */
-int _strcmp(const char *s1, const char *s2);
-/* shell.h */
-char *_strchr(const char *s, int c);
-char *find_command(char *command);
 int has_path_env(void);
-void execute_command(char *command);
-int is_interactive(void);
-char *_getpath(void);
+char **parse_line(char *line);
+int execute_cmd(char **args);
+extern char **environ;
+char *find_command(char *command);
 
-#endif /* SHELL_H */
+
+
+
+#endif
