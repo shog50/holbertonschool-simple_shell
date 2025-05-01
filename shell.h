f68b1068b3c3d1757ab9a12b25b970f295dd7e8a
rwwwn <ralbaraiki@gmail.com>
2025-05-01 13:21:40 +0000

    task4

--- shell.h
+++ shell.h
@@ -1,24 +1,19 @@
 #ifndef SHELL_H
 #define SHELL_H
-#define MAX_ARGS 64
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <stddef.h>
 #include <string.h>
-#include <sys/types.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
-void run_shell(void);
-ssize_t _getline(char **lineptr, size_t *n);
+int has_path_env(void);
+char **parse_line(char *line);
+int execute_cmd(char **args);
 extern char **environ;
-int _strcmp(const char *s1, const char *s2);
-char *_strchr(const char *s, int c);
 char *find_command(char *command);
-int is_interactive(void);
-char *_strdup(const char *str);
-char *_strcat(char *dest, const char *src);
-int _strlen(const char *s);
-int _strncmp(const char *s1, const char *s2, size_t n);
+
+
+
 
 #endif
